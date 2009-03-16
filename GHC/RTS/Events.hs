b5eab38241e1c8cd33f02dc1353eb3aef46c32f2
Simon Marlow <marlowsd@gmail.com>
2009-03-16 14:43:30 +0000

    Various updates  - include variable-sized events  - don't fail on unrecognised events  - various naming changes

--- GHC/RTS/Events.hs
+++ GHC/RTS/Events.hs
@@ -5,13 +5,25 @@
  -   Parser functions for GHC RTS EventLog framework.
  -}
  
-module GHC.RTS.Events where
+module GHC.RTS.Events (
+       EventLog(..),
+       EventType(..),
+       Event(..),
+       EventTypeSpecificInfo(..),
+       ThreadStopStatus(..),
+       Header(..),
+       Data(..),
+       readEventLogFromFile,
+  ) where
 
 {- Libraries. -}
 import Data.Word (Word16, Word32, Word64)
 import Debug.Trace
 import Data.Binary
 import Control.Monad
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as M
+import Control.Monad.Reader
 
 #define EVENTLOG_CONSTANTS_ONLY
 #include "EventLogFormat.h"
@@ -20,7 +32,7 @@ import Control.Monad
 type Filename = String
 
 -- EventType.
-type EventTypeNum = Word32
+type EventTypeNum = Word16
 type EventTypeDescLen = Word32
 type EventTypeDesc = String
 -- Event.
@@ -30,19 +42,39 @@ type ThreadId = Word64
 
 type Marker = Word32
 
-{- Data Types. -}
-data EventType = 
+{-
+ - Data type delcarations to build the GHC RTS data format,
+ - which is a (header, data) pair.
+ -
+ - Header contains EventTypes.
+ - Data contains Events.
+ -}
+data EventLog =
+  EventLog {
+    header :: Header,
+    dat    :: Data
+  } deriving Show
+
+newtype Header = Header {
+     eventTypes :: [EventType]
+  } deriving Show
+
+data Data = Data {
+     events :: [Event]
+  } deriving Show
+
+data EventType =
   EventType {
-    etNum  :: EventTypeNum,
-    etDesc :: EventTypeDesc,
-    etSize :: Int
+    num  :: EventTypeNum,
+    desc :: EventTypeDesc,
+    size :: Maybe Word16 -- ^ 'Nothing' indicates variable size
   } deriving Show
 
 data Event = 
   Event {
-    etRef :: EventTypeNum,
-    ts    :: Timestamp,
-    spec  :: EventTypeSpecificInfo
+    ref  :: EventTypeNum,
+    time :: Timestamp,
+    spec :: EventTypeSpecificInfo
   } deriving Show
 
 data EventTypeSpecificInfo
@@ -59,6 +91,7 @@ data EventTypeSpecificInfo
   | RequestParGC   { cap :: Int }
   | StartGC        { cap :: Int }
   | EndGC          { cap :: Int }
+  | UnknownEvent
   deriving Show
 
 --sync with ghc/includes/Constants.h
@@ -72,34 +105,44 @@ data ThreadStopStatus
  | ForeignCall
  deriving (Enum, Show)
 
-newtype Header = Header EventTypes
-     deriving Show
-
-newtype EventTypes = EventTypes [EventType]
-     deriving Show
+-- reader/Get monad that passes around the event types
+type GetEventLog a = ReaderT (IntMap EventType) Get a
   
-------------------------------------------------------------------------------
+getT :: (MonadTrans m, Binary a) => m Get a
+getT = lift get
+
+-- -----------------------------------------------------------------------------
 -- Binary instances
+
 instance Binary EventType where
   put (EventType etNum etDescLen etDesc) = do return () -- TODO.
   get = do etNum <- get 
            etDescLen <- get :: Get EventTypeDescLen
            etDesc <- getEtDesc (fromIntegral etDescLen) 
-           etSize <- get :: Get Word16
+           size <- get :: Get Word16
+           -- 0xffff indicates variable-sized event
+           let etSize = if size == 0xffff then Nothing else Just size
            ete <- get :: Get Marker
            when (ete /= EVENT_ET_END) $
               error ("Event Type end marker not found.")
-           return (EventType etNum etDesc (fromIntegral etSize))
+           return (EventType etNum etDesc etSize)
            where 
              getEtDesc :: Int -> Get [Char]
              getEtDesc s = replicateM s (get :: Get Char)
 
-instance Binary EventTypes where
-  put (EventTypes ets) = do return () -- TODO.
-  get = do hetm <- get :: Get Marker
+instance Binary Header where
+  put (Header ets) = do return () -- TODO.
+  get = do hdrb <- get :: Get Marker
+           when (hdrb /= EVENT_HEADER_BEGIN) $
+                error "Header begin marker not found."
+           hetm <- get :: Get Marker
            when (hetm /= EVENT_HET_BEGIN) $ 
                 error "Header Event Type begin marker not found."
-           EventTypes `fmap` getEventTypes
+           ets <- getEventTypes
+           emark <- get :: Get Marker
+           when (emark /= EVENT_HEADER_END) $
+                error "Header end marker not found."
+           return (Header ets)
      where    
        getEventTypes :: Get [EventType]
        getEventTypes = do
@@ -114,116 +157,114 @@ instance Binary EventTypes where
               | otherwise ->
                    error "Malformed list of Event Types in header."
 
-instance Binary Header where
-  put (Header ets) = do return () -- TODO.
-  get = do hdrb <- get :: Get Marker
-           when (hdrb /= EVENT_HEADER_BEGIN) $ 
-                error "Header begin marker not found."
-           ets <- get
-           emark <- get :: Get Marker
-           when (emark /= EVENT_HEADER_END) $ 
-                error "Header end marker not found."
-           return (Header ets)
-
-getEvent :: Get (Maybe Event)
+getEvent :: GetEventLog (Maybe Event)
 getEvent = do 
-  etRef <- get
+  etRef <- getT
   if (etRef == EVENT_DATA_END) 
      then return Nothing
-     else do ts   <- get
+     else do ts   <- getT
              spec <- getEvSpecInfo etRef
              return (Just (Event etRef ts spec))
            
-getEvSpecInfo :: EventTypeNum -> Get EventTypeSpecificInfo
-getEvSpecInfo EVENT_CREATE_THREAD = do  -- (cap, thread)
-  c  <- get :: Get Word16
-  t <- get
+getEvSpecInfo :: EventTypeNum -> GetEventLog EventTypeSpecificInfo
+getEvSpecInfo num = case num of
+
+ EVENT_CREATE_THREAD -> do  -- (cap, thread)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
   return CreateThread{cap=fromIntegral c,thread=t}
-getEvSpecInfo EVENT_RUN_THREAD = do  -- (cap, thread)
-  c  <- get :: Get Word16
-  t <- get
+
+ EVENT_RUN_THREAD -> do  --  (cap, thread)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
   return RunThread{cap=fromIntegral c,thread=t}
-getEvSpecInfo EVENT_STOP_THREAD = do  -- (cap, thread, status)
-  c  <- get :: Get Word16
-  t <- get
-  s <- get :: Get Word16
-  return StopThread{cap=fromIntegral c,
-                    thread=t, 
-                    status= toEnum (fromIntegral s)}
-getEvSpecInfo EVENT_THREAD_RUNNABLE = do  -- (cap, thread)
-  c  <- get :: Get Word16
-  t <- get
+
+ EVENT_STOP_THREAD -> do  -- (cap, thread, status)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
+  s <- getT :: GetEventLog Word16
+  return StopThread{cap=fromIntegral c,thread=t, status= toEnum (fromIntegral s)}
+
+ EVENT_THREAD_RUNNABLE -> do  -- (cap, thread)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
   return ThreadRunnable{cap=fromIntegral c,thread=t}
-getEvSpecInfo EVENT_MIGRATE_THREAD = do  -- (cap, thread, new_cap)
-  c  <- get :: Get Word16
-  t <- get
-  c <- get :: Get Word16
+
+ EVENT_MIGRATE_THREAD -> do  --  (cap, thread, new_cap)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
+  c <- getT :: GetEventLog Word16
   return MigrateThread{cap=fromIntegral c,thread=t,newCap=fromIntegral c}
-getEvSpecInfo EVENT_RUN_SPARK = do  -- (cap, thread)
-  c  <- get :: Get Word16
-  t <- get
+
+ EVENT_RUN_SPARK -> do  -- (cap, thread)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
   return RunSpark{cap=fromIntegral c,thread=t}
-getEvSpecInfo EVENT_STEAL_SPARK = do  -- (cap, thread, victim_cap)
-  c  <- get :: Get Word16
-  t <- get
-  c <- get :: Get Word16
+
+ EVENT_STEAL_SPARK -> do  -- (cap, thread, victim_cap)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
+  c <- getT :: GetEventLog Word16
   return StealSpark{cap=fromIntegral c,thread=t,origCap=fromIntegral c}
-getEvSpecInfo EVENT_SHUTDOWN = do  -- (cap)
-  c  <- get :: Get Word16
+
+ EVENT_SHUTDOWN -> do  -- (cap)
+  c  <- getT :: GetEventLog Word16
   return Shutdown{cap=fromIntegral c}
-getEvSpecInfo EVENT_THREAD_WAKEUP = do  -- (cap, thread, other_cap)
-  c  <- get :: Get Word16
-  t <- get
-  c <- get :: Get Word16
+
+ EVENT_THREAD_WAKEUP -> do  -- (cap, thread, other_cap)
+  c  <- getT :: GetEventLog Word16
+  t <- getT
+  c <- getT :: GetEventLog Word16
   return WakeupThread{cap=fromIntegral c,thread=t,otherCap=fromIntegral c}
-getEvSpecInfo EVENT_REQUEST_SEQ_GC = do  -- (cap)
-  c  <- get :: Get Word16
+
+ EVENT_REQUEST_SEQ_GC -> do  -- (cap)
+  c  <- getT :: GetEventLog Word16
   return RequestSeqGC{cap=fromIntegral c}
-getEvSpecInfo EVENT_REQUEST_PAR_GC = do  -- (cap)
-  c  <- get :: Get Word16
+
+ EVENT_REQUEST_PAR_GC -> do  -- (cap)
+  c  <- getT :: GetEventLog Word16
   return RequestParGC{cap=fromIntegral c}
-getEvSpecInfo EVENT_GC_START = do  -- (cap)
-  c  <- get :: Get Word16
+
+ EVENT_GC_START -> do  -- (cap)
+  c  <- getT :: GetEventLog Word16
   return StartGC{cap=fromIntegral c}
-getEvSpecInfo EVENT_GC_END = do  -- (cap)
-  c  <- get :: Get Word16
+
+ EVENT_GC_END -> do  -- (cap)
+  c  <- getT :: GetEventLog Word16
   return EndGC{cap=fromIntegral c}
-getEvSpecInfo other = 
-  error ("getEvSpecInfo: unknown event type: " ++ show other)
 
-data Data = Data {
-    events :: [Event]
-  } deriving Show
-          
-instance Binary Data where
-  put (Data { events = [] }) = do return () -- TODO.
-  get = 
-    do db <- get :: Get Marker 
-       when (db /= EVENT_DATA_BEGIN) $ error "Data begin marker not found."
-       getEvents [] 
-    where
-      getEvents :: [Event] -> Get Data
-      getEvents events = 
-        do mb_e <- getEvent
-           case mb_e of
-             Nothing -> return (Data events)
-             Just e  -> getEvents (e:events)
+ other -> do -- unrecognised event, just skip it
+  etypes <- ask
+  case M.lookup (fromIntegral other) etypes of
+    Nothing -> error ("getEvSpecInfo: undeclared event type: " ++ show other)
+    Just t  -> do
+      bytes <- case size t of
+                 Just n  -> return n
+                 Nothing -> getT :: GetEventLog Word16
+      skip  <- lift $ replicateM_ (fromIntegral bytes) getWord8
+      return UnknownEvent
 
--- Format defines the layout of the trace file.
-data Format = 
-  Format {
-    fmtHeader :: Header,
-    fmtData :: Data
-  } deriving Show
 
-instance Binary Format where
-  put (Format hdr dat) = do return () -- TODO.
-  get = do hdr <- get :: Get Header
-           dat <- get :: Get Data
-           return (Format hdr dat)
+getData :: GetEventLog Data
+getData = do
+   db <- getT :: GetEventLog Marker
+   when (db /= EVENT_DATA_BEGIN) $ error "Data begin marker not found."
+   getEvents []
+   where
+              getEvents :: [Event] -> GetEventLog Data
+              getEvents events = do
+                mb_e <- getEvent
+                case mb_e of
+                  Nothing -> return (Data events)
+                  Just e  -> getEvents (e:events)
 
-{- Functions. -}
-buildFormat :: FilePath -> IO Format
-buildFormat = decodeFile
+instance Binary EventLog where
+  put = undefined -- Todo
+  get = do
+    header <- get :: Get Header
+    let imap = M.fromList [ (fromIntegral (num t),t) | t <- eventTypes header]
+    dat <- runReaderT getData imap
+    return (EventLog header dat)
 
-{- EOF. -}
+readEventLogFromFile :: FilePath -> IO EventLog
+readEventLogFromFile = decodeFile
