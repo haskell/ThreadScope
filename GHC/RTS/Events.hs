c21d84d5dd64f7f3012a0deb4d570ab854ad95a6
Simon Marlow <marlowsd@gmail.com>
2009-03-17 16:24:21 +0000

    detect and propagate parsing errors properly

--- GHC/RTS/Events.hs
+++ GHC/RTS/Events.hs
@@ -20,10 +20,13 @@ module GHC.RTS.Events (
 import Data.Word (Word16, Word32, Word64)
 import Debug.Trace
 import Data.Binary
+import Data.Binary.Get
 import Control.Monad
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as M
 import Control.Monad.Reader
+import Control.Monad.Error
+import qualified Data.ByteString.Lazy as L
 
 #define EVENTLOG_CONSTANTS_ONLY
 #include "EventLogFormat.h"
@@ -107,167 +110,173 @@ data ThreadStopStatus
  deriving (Enum, Show)
 
 -- reader/Get monad that passes around the event types
-type GetEventLog a = ReaderT (IntMap EventType) Get a
+type GetEvents a = ReaderT (IntMap EventType) (ErrorT String Get) a
   
-getT :: (MonadTrans m, Binary a) => m Get a
-getT = lift get
+type GetHeader a = ErrorT String Get a
+
+getH :: Binary a => GetHeader a
+getH = lift get
+
+getE :: Binary a => GetEvents a
+getE = lift . lift $ get
 
 ------------------------------------------------------------------------------
 -- Binary instances
 
-instance Binary EventType where
-  put (EventType etNum etDescLen etDesc) = do return () -- TODO.
-  get = do etNum <- get 
-           etDescLen <- get :: Get EventTypeDescLen
+getEventType :: GetHeader EventType
+getEventType = do 
+           etNum <- getH
+           etDescLen <- getH :: GetHeader EventTypeDescLen
            etDesc <- getEtDesc (fromIntegral etDescLen) 
-           size <- get :: Get EventTypeSize
+           size <- getH :: GetHeader EventTypeSize
            -- 0xffff indicates variable-sized event
            let etSize = if size == 0xffff then Nothing else Just size
-           ete <- get :: Get Marker
+           ete <- getH :: GetHeader Marker
            when (ete /= EVENT_ET_END) $
-              error ("Event Type end marker not found.")
+              throwError ("Event Type end marker not found.")
            return (EventType etNum etDesc etSize)
            where 
-             getEtDesc :: Int -> Get [Char]
-             getEtDesc s = replicateM s (get :: Get Char)
+             getEtDesc :: Int -> GetHeader [Char]
+             getEtDesc s = replicateM s (getH :: GetHeader Char)
 
-instance Binary Header where
-  put (Header ets) = do return () -- TODO.
-  get = do hdrb <- get :: Get Marker
+getHeader :: GetHeader Header
+getHeader = do 
+           hdrb <- getH :: GetHeader Marker
            when (hdrb /= EVENT_HEADER_BEGIN) $
-                error "Header begin marker not found."
-           hetm <- get :: Get Marker
+                throwError "Header begin marker not found"
+           hetm <- getH :: GetHeader Marker
            when (hetm /= EVENT_HET_BEGIN) $ 
-                error "Header Event Type begin marker not found."
+                throwError "Header Event Type begin marker not found"
            ets <- getEventTypes
-           emark <- get :: Get Marker
+           emark <- getH :: GetHeader Marker
            when (emark /= EVENT_HEADER_END) $
-                error "Header end marker not found."
+                throwError "Header end marker not found"
            return (Header ets)
      where    
-       getEventTypes :: Get [EventType]
+       getEventTypes :: GetHeader [EventType]
        getEventTypes = do
-           m <- get :: Get Marker
+           m <- getH :: GetHeader Marker
            case () of 
             _ | m == EVENT_ET_BEGIN -> do
-                   et <- get :: Get EventType
+                   et <- getEventType
                    nextET <- getEventTypes
                    return (et : nextET)
               | m == EVENT_HET_END ->
                    return []
               | otherwise ->
-                   error "Malformed list of Event Types in header."
+                   throwError "Malformed list of Event Types in header"
 
-getEvent :: GetEventLog (Maybe Event)
+getEvent :: GetEvents (Maybe Event)
 getEvent = do 
-  etRef <- getT
+  etRef <- getE
   if (etRef == EVENT_DATA_END) 
      then return Nothing
-     else do ts   <- getT
+     else do ts   <- getE
              spec <- getEvSpecInfo etRef
              return (Just (Event etRef ts spec))
            
-getEvSpecInfo :: EventTypeNum -> GetEventLog EventTypeSpecificInfo
+getEvSpecInfo :: EventTypeNum -> GetEvents EventTypeSpecificInfo
 getEvSpecInfo num = case num of
 
  EVENT_CREATE_THREAD -> do  -- (cap, thread)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
+  c  <- getE :: GetEvents Word16
+  t <- getE
   return CreateThread{cap=fromIntegral c,thread=t}
 
  EVENT_RUN_THREAD -> do  --  (cap, thread)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
+  c  <- getE :: GetEvents Word16
+  t <- getE
   return RunThread{cap=fromIntegral c,thread=t}
 
  EVENT_STOP_THREAD -> do  -- (cap, thread, status)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
-  s <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
+  t <- getE
+  s <- getE :: GetEvents Word16
   return StopThread{cap=fromIntegral c,thread=t, status= toEnum (fromIntegral s)}
 
  EVENT_THREAD_RUNNABLE -> do  -- (cap, thread)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
+  c  <- getE :: GetEvents Word16
+  t <- getE
   return ThreadRunnable{cap=fromIntegral c,thread=t}
 
  EVENT_MIGRATE_THREAD -> do  --  (cap, thread, new_cap)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
-  c <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
+  t <- getE
+  c <- getE :: GetEvents Word16
   return MigrateThread{cap=fromIntegral c,thread=t,newCap=fromIntegral c}
 
  EVENT_RUN_SPARK -> do  -- (cap, thread)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
+  c  <- getE :: GetEvents Word16
+  t <- getE
   return RunSpark{cap=fromIntegral c,thread=t}
 
  EVENT_STEAL_SPARK -> do  -- (cap, thread, victim_cap)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
-  c <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
+  t <- getE
+  c <- getE :: GetEvents Word16
   return StealSpark{cap=fromIntegral c,thread=t,origCap=fromIntegral c}
 
  EVENT_SHUTDOWN -> do  -- (cap)
-  c  <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
   return Shutdown{cap=fromIntegral c}
 
  EVENT_THREAD_WAKEUP -> do  -- (cap, thread, other_cap)
-  c  <- getT :: GetEventLog Word16
-  t <- getT
-  c <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
+  t <- getE
+  c <- getE :: GetEvents Word16
   return WakeupThread{cap=fromIntegral c,thread=t,otherCap=fromIntegral c}
 
  EVENT_REQUEST_SEQ_GC -> do  -- (cap)
-  c  <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
   return RequestSeqGC{cap=fromIntegral c}
 
  EVENT_REQUEST_PAR_GC -> do  -- (cap)
-  c  <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
   return RequestParGC{cap=fromIntegral c}
 
  EVENT_GC_START -> do  -- (cap)
-  c  <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
   return StartGC{cap=fromIntegral c}
 
  EVENT_GC_END -> do  -- (cap)
-  c  <- getT :: GetEventLog Word16
+  c  <- getE :: GetEvents Word16
   return EndGC{cap=fromIntegral c}
 
  other -> do -- unrecognised event, just skip it
   etypes <- ask
   case M.lookup (fromIntegral other) etypes of
-    Nothing -> error ("getEvSpecInfo: undeclared event type: " ++ show other)
+    Nothing -> throwError ("getEvSpecInfo: undeclared event type: " ++ show other)
     Just t  -> do
       bytes <- case size t of
                  Just n  -> return n
-                 Nothing -> getT :: GetEventLog Word16
-      skip  <- lift $ replicateM_ (fromIntegral bytes) getWord8
+                 Nothing -> getE :: GetEvents Word16
+      skip  <- lift . lift $ replicateM_ (fromIntegral bytes) getWord8
       return UnknownEvent
 
 
-getData :: GetEventLog Data
+getData :: GetEvents Data
 getData = do
-   db <- getT :: GetEventLog Marker
-   when (db /= EVENT_DATA_BEGIN) $ error "Data begin marker not found."
+   db <- getE :: GetEvents Marker
+   when (db /= EVENT_DATA_BEGIN) $ throwError "Data begin marker not found"
    getEvents []
    where
-              getEvents :: [Event] -> GetEventLog Data
+              getEvents :: [Event] -> GetEvents Data
               getEvents events = do
                 mb_e <- getEvent
                 case mb_e of
                   Nothing -> return (Data events)
                   Just e  -> getEvents (e:events)
 
-instance Binary EventLog where
-  put = undefined -- Todo
-  get = do
-    header <- get :: Get Header
+getEventLog :: ErrorT String Get EventLog
+getEventLog = do
+    header <- getHeader
     let imap = M.fromList [ (fromIntegral (num t),t) | t <- eventTypes header]
     dat <- runReaderT getData imap
     return (EventLog header dat)
 
-readEventLogFromFile :: FilePath -> IO EventLog
-readEventLogFromFile = decodeFile
-
-{- EOF. -}
+readEventLogFromFile :: FilePath -> IO (Either String EventLog)
+readEventLogFromFile f = do
+    s <- L.readFile f
+    return $ runGet (do v <- runErrorT $ getEventLog
+                        m <- isEmpty
+                        m `seq` return v)  s
--- ShowEvents.hs
+++ ShowEvents.hs
@@ -1,59 +1,81 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import GHC.RTS.Events
+import GHC.RTS.Events as Log
 import System.Environment
 import Text.Printf
 import Data.List
 import Data.Function
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as M
 import Data.Maybe
+import System.IO
+import System.Exit
 
 main = do
   [file] <- getArgs
 
-  eventLog <- readEventLogFromFile file
-
-  let ets = eventTypes $ header eventLog
-      es = events $ dat eventLog
-      sorted_es = sortBy (compare `on` time) es
-
+  log <- do e <- readEventLogFromFile file
+            case e of
+               Left  s   -> die ("Failed to parse " ++ file ++ ": " ++ s)
+               Right log -> return log
+    
   printf "Event Types:\n"
-  putStrLn (unlines (map ppEventType ets))
+  let etypes = Log.eventTypes (Log.header log)
+  putStrLn (unlines (map ppEventType etypes))
+
+  let imap = M.fromList [ (fromIntegral (Log.num t),t) | t <- etypes ]
+
+  let pes = Log.events (Log.dat log)
+      sorted = sortBy (compare `on` Log.time) (reverse pes)
+              -- the events come out reversed, and we want a stable sort
 
   printf "Events:\n"
-  putStrLn $ unlines $ map ppEvent $ sorted_es
+  putStrLn $ unlines $ map (ppEvent imap) $ sorted
+
+
+die s = do hPutStrLn stderr s; exitWith (ExitFailure 1)
+
+--   putStrLn (show $ getFirstPE dat)
+--   let len = length $ phaseEvents dat
+--   putStrLn (show $ phaseEvents dat !! (len - 3))
+--   putStrLn (show $ phaseEvents dat !! (len - 2))
+--   putStrLn (show $ phaseEvents dat !! (len - 1))
+-- 
+-- getFirstPE dat = head $ phaseEvents dat
 
 ppEventType :: EventType -> String
-ppEventType et = 
-  printf "%4d: %s (size %d)" (num et) (desc et) (varSize et)
-  where
-    varSize et = case (size et) of 
-                   Just sz -> sz
-                   Nothing -> 0
+ppEventType et = printf "%4d: %s (size %s)" (Log.num et) (Log.desc et) 
+   (case Log.size et of Nothing -> "variable"; Just x -> show x)
 
-ppEvent :: Event -> String
-ppEvent Event{..} =
-  printf "%9d: cap %d: " time (cap spec) ++
+ppEvent :: IntMap EventType -> Event -> String
+ppEvent imap Event{..} =
+  printf "%9d: " time ++
   case spec of
-    CreateThread{..}   -> printf "creating thread %d" thread
-    RunThread{..}      -> printf "running thread %d" thread
-    StopThread{..}     -> printf "stopping thread %d (%s)" thread (showThreadStopStatus status)
-    ThreadRunnable{..} -> printf "thread %d is runnable" thread
-    MigrateThread{..}  -> printf "migrating thread %d to cap %d" thread newCap
-    RunSpark{..}       -> printf "running a local spark (thread %d)" thread
-    StealSpark{..}     -> printf "thread %d stealing a spark from cap %d" thread origCap
-    Shutdown{..}       -> printf "shutting down"
-    WakeupThread{..}   -> printf "waking up thread %d on cap %d" thread otherCap
-    RequestSeqGC{..}   -> printf "requesting sequential GC"
-    RequestParGC{..}   -> printf "requesting parallel GC"
-    StartGC{..}        -> printf "starting GC"
-    EndGC{..}          -> printf "finished GC"
-   
+    UnknownEvent ->
+      printf (Log.desc (fromJust (M.lookup (fromIntegral ref) imap)))
+
+    other ->
+      printf "cap %d: " (cap spec) ++
+      case spec of
+        CreateThread{..}   -> printf "creating thread %d" thread
+        RunThread{..}      -> printf "running thread %d" thread
+        StopThread{..}     -> printf "stopping thread %d (%s)" thread (showThreadStopStatus status)
+        ThreadRunnable{..} -> printf "thread %d is runnable" thread
+        MigrateThread{..}  -> printf "migrating thread %d to cap %d" thread newCap
+        RunSpark{..}       -> printf "running a local spark (thread %d)" thread
+        StealSpark{..}     -> printf "thread %d stealing a spark from cap %d" thread origCap
+        Shutdown{..}       -> printf "shutting down"
+        WakeupThread{..}   -> printf "waking up thread %d on cap %d" thread otherCap
+        RequestSeqGC{..}   -> printf "requesting sequential GC"
+        RequestParGC{..}   -> printf "requesting parallel GC"
+        StartGC{..}        -> printf "starting GC"
+        EndGC{..}          -> printf "finished GC"
+
 showThreadStopStatus :: ThreadStopStatus -> String
 showThreadStopStatus HeapOverflow   = "heap overflow"
 showThreadStopStatus StackOverflow  = "stack overflow"
 showThreadStopStatus ThreadYielding = "thread yielding"
 showThreadStopStatus ThreadBlocked  = "thread blocked"
 showThreadStopStatus ThreadFinished = "thread finished"
-
-{- EOF. -}
+showThreadStopStatus ForeignCall    = "making a foreign call"
