ac11aab7342d0aa0948f11ce1720bdbf0725360a
Simon Marlow <marlowsd@gmail.com>
2009-03-25 12:26:45 +0000

    update to the latest EventLogFormat from GHC

--- GHC/RTS/EventLogFormat.h
+++ GHC/RTS/EventLogFormat.h
@@ -12,7 +12,26 @@
  * Each event has a specific format.  If you add new events, give them
  * new numbers: we never re-use old event numbers.
  *
- * The format is:
+ * - The format is endian-independent: all values are represented in 
+ *    bigendian order.
+ *
+ * - The format is extensible:
+ *
+ *    - The header describes each event type and its length.  Tools
+ *      that don't recognise a particular event type can skip those events.
+ *
+ *    - There is room for extra information in the event type
+ *      specification, which can be ignored by older tools.
+ *
+ *    - Events can have extra information added, but existing fields
+ *      cannot be changed.  Tools should ignore extra fields at the
+ *      end of the event record.
+ *
+ *    - Old event type ids are never re-used; just take a new identifier.
+ *
+ *
+ * The format
+ * ----------
  *
  * log : EVENT_HEADER_BEGIN
  *       EventType*
@@ -23,13 +42,20 @@
  *
  * EventType :
  *       EVENT_ET_BEGIN
- *       EventTypeNum      -- unique identifier for this event
- *       Word32 (= len)    
- *       Word8[len]        -- string describing the event
- *       Word16            -- size of the event in bytes (minus the header)
+ *       Word16         -- unique identifier for this event
+ *       Int16          -- >=0  size of the event in bytes (minus the header)
+ *                      -- -1   variable size
+ *       Word32         -- length of the next field in bytes
+ *       Word8*         -- string describing the event
+ *       Word32         -- length of the next field in bytes
+ *       Word8*         -- extra info (for future extensions)
  *       EVENT_ET_END
  *
- * Event : (see struct definitions below)
+ * Event : 
+ *       Word16         -- event_type
+ *       Word64         -- time (nanosecs)
+ *       [Word16]       -- length of the rest (for variable-sized events only)
+ *       ... extra event-specific info ...
  *
  *
  * To add a new event
@@ -40,7 +66,7 @@
  *  - In EventLog.c
  *    - add it to the EventDesc array
  *    - emit the event type in initEventLogging()
- *    - emit the new event in psotEvent_()
+ *    - emit the new event in postEvent_()
  *    - generate the event itself by calling postEvent() somewhere
  *  - In the Haskell code to parse the event log file:
  *    - add types and code to read the new event
@@ -49,14 +75,12 @@
 
 /*
  * Markers for begin/end of the Header.
- * Header, Begin = hdrb
- * Header, End = hdre
  */
 #define EVENT_HEADER_BEGIN    0x68647262 /* 'h' 'd' 'r' 'b' */
 #define EVENT_HEADER_END      0x68647265 /* 'h' 'd' 'r' 'e' */
 
 #define EVENT_DATA_BEGIN      0x64617462 /* 'd' 'a' 't' 'b' */
-#define EVENT_DATA_END        0xffffffff
+#define EVENT_DATA_END        0xffff
 
 /*
  * Markers for begin/end of the list of Event Types in the Header.
@@ -103,7 +127,7 @@
 
 #ifndef EVENTLOG_CONSTANTS_ONLY
 
-typedef StgWord32 EventTypeNum;
+typedef StgWord16 EventTypeNum;
 typedef StgWord64 Timestamp; // in nanoseconds
 typedef StgWord64 ThreadID;
 typedef StgWord16 CapabilityNum;
