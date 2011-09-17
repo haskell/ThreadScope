87dfb10743ff4f153598c7887560261b2251542a
Mikolaj Konarski <mikolaj@well-typed.com>
2011-09-17 01:22:17 +0000

    Add grey background in PDF/PNG and so workaround the scroll bug

--- GUI/SaveAs.hs
+++ GUI/SaveAs.hs
@@ -8,6 +8,7 @@ import Events.HECs
 -- Imports for GTK
 import Graphics.UI.Gtk
 import Graphics.Rendering.Cairo
+import GUI.ViewerColours
 
 -------------------------------------------------------------------------------
 
@@ -16,9 +17,19 @@ saveAsPDF file hecs viewParams =
 
     withPDFSurface file w' h' $ \surface ->
       renderWith surface $ do
-        renderLabelArea viewParams hecs
+        -- TODO: should be
+        -- renderLabelArea viewParams hecs
+        -- translate (fromIntegral label_area_width) 0
+        -- renderTraces viewParams hecs (Rectangle 0 0 w h)
+        -- TODO: but renderTraces draws to the left of 0 (not seen on screen)
+        -- Workaround:
         translate (fromIntegral label_area_width) 0
         renderTraces viewParams hecs (Rectangle 0 0 w h)
+        translate (fromIntegral (-label_area_width)) 0
+        rectangle 0 0 (fromIntegral label_area_width) (fromIntegral h)
+        setSourceRGBAhex gtkBorderGrey 1
+        fill
+        renderLabelArea viewParams hecs
   where
     w = width  viewParams; w' = fromIntegral $ label_area_width + w
     h = height viewParams; h' = fromIntegral $ label_area_width + h
@@ -27,19 +38,23 @@ saveAsPDF file hecs viewParams =
 
 -------------------------------------------------------------------------------
 
+-- TODO: the same issues as above
 saveAsPNG :: FilePath -> HECs -> ViewParameters -> IO ()
 saveAsPNG file hecs viewParams =
 
     withImageSurface FormatARGB32 w' h' $ \surface -> do
       renderWith surface $ do
-        renderLabelArea viewParams hecs
         translate (fromIntegral label_area_width) 0
         renderTraces viewParams hecs (Rectangle 0 0 w h)
+        translate (fromIntegral (-label_area_width)) 0
+        rectangle 0 0 (fromIntegral label_area_width) (fromIntegral h)
+        setSourceRGBAhex gtkBorderGrey 1
+        fill
+        renderLabelArea viewParams hecs
       surfaceWriteToPNG surface file
   where
     w = width  viewParams; w' = fromIntegral $ label_area_width + w
     h = height viewParams; h' = fromIntegral $ label_area_width + h
-    -- TODO: take from the .ui file, from timeline_labels_drawingarea:
     label_area_width = 110
 
 -------------------------------------------------------------------------------
