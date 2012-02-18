b42ff57250df38a86d24f1426899f1cf34e30942
Mikolaj Konarski <mikolaj@well-typed.com>
2012-02-18 16:46:04 +0100

    Almost all of +RTS -s simulated

--- GUI/SummaryView.hs
+++ GUI/SummaryView.hs
@@ -16,6 +16,7 @@ import Graphics.Rendering.Cairo
 import Data.Array
 import Data.IORef
 import Data.Maybe
+import Data.Word (Word64)
 import qualified Data.List as L
 import qualified Data.IntMap as IM
 import Control.Exception (assert)
@@ -86,14 +87,33 @@ genericSetEvents processEvents SummaryView{..} mev = do
   writeIORef mintervalIORef Nothing  -- the old interval may make no sense
   widgetQueueDraw gtkLayout
 
-data RtsSparkCounters = RtsSparkCounters
+data SummaryData = SummaryData
+  { dallocated     :: Maybe (Word64, Word64)
+  , dcopied        :: Maybe Word64
+  , dmaxResidency  :: Maybe Word64
+  , dmaxSlop       :: Maybe Word64
+  , dmaxMemory     :: Maybe Word64
+  , dmaxFrag       :: Maybe Word64
+  , dGCTable       :: !(IM.IntMap RtsGC)
+  , dparBalance    :: Maybe Double
+--, dtaskTable     -- of questionable usefulness, hard to get
+  , dsparkTable    :: !(IM.IntMap (RtsSpark, RtsSpark))
+--, dInitExitT     -- naturally excluded from eventlog
+  , dMutTime       :: Maybe Double
+  , dGCTime        :: Maybe Double
+  , dtotalTime     :: Maybe Double
+  , dallocRate     :: Maybe Int
+  , dproductivity  :: Maybe Double
+  }
+
+data RtsSpark = RtsSpark
  { sparkCreated, sparkDud, sparkOverflowed
  , sparkConverted, sparkFizzled, sparkGCd :: !Timestamp
  }
 
 data GcMode = ModePar | ModeSeq | ModeInitial | ModeEnded
 
-data RtsGCCounters = RtsGCCounters
+data RtsGC = RtsGC
   { gcMode      :: !GcMode
   , gcLastEvent :: !EventInfo
   , gcLastStart :: !Timestamp
@@ -103,133 +123,95 @@ data RtsGCCounters = RtsGCCounters
   , gcMaxPause  :: !Timestamp
   }
 
-data RtsState = RtsState
-  { rtsGC     :: !(IM.IntMap RtsGCCounters)
-  , rtsSparks :: !(IM.IntMap (RtsSparkCounters, RtsSparkCounters))
+emptySummaryData :: SummaryData
+emptySummaryData = SummaryData
+  { dallocated     = Nothing
+  , dcopied        = Nothing
+  , dmaxResidency  = Nothing
+  , dmaxSlop       = Nothing
+  , dmaxMemory     = Nothing
+  , dmaxFrag       = Nothing
+  , dGCTable       = IM.empty
+  , dparBalance    = Nothing
+  , dsparkTable    = IM.empty
+  , dMutTime       = Nothing
+  , dGCTime        = Nothing
+  , dtotalTime     = Nothing
+  , dallocRate     = Nothing
+  , dproductivity  = Nothing
   }
 
--- TODO: Split up some more.
-summaryViewProcessEvents :: Maybe Interval -> Maybe (Array Int CapEvent)
-                         -> (String, Maybe (Array Int CapEvent))
-summaryViewProcessEvents _ Nothing = ("", Nothing)
-summaryViewProcessEvents minterval (Just events) =
-  let start = RtsState
-        { rtsGC    = IM.empty
-        , rtsSparks = IM.empty
-        }
-      RtsState{rtsGC, rtsSparks = rtsSparksRaw} =
-        L.foldl' step start $ elems $ events
-      diffSparks (RtsSparkCounters crt1 dud1 ovf1 cnv1 fiz1 gcd1,
-                  RtsSparkCounters crt2 dud2 ovf2 cnv2 fiz2 gcd2) =
-        RtsSparkCounters (crt2 - crt1) (dud2 - dud1) (ovf2 - ovf1)
-                         (cnv2 - cnv1) (fiz2 - fiz1) (gcd2 - gcd1)
-      rtsSparks = IM.map diffSparks rtsSparksRaw
-      totalElapsedS = timeToSecondsDbl $ iend - istart
-      gcLine :: Int -> RtsGCCounters -> String
-      gcLine k = displayGCCounter (printf "GC HEC %d" k)
-      gcSum = sumGCCounters $ IM.elems rtsGC
-      gcLines =
-        [ (-300,        "                                            Tot elapsed time   Avg pause  Max pause")] ++
-        (map (\ (k, gc) -> (-200 + k, gc)) $
-           IM.assocs (IM.mapWithKey gcLine rtsGC)) ++
-        [(-100, displayGCCounter "GC TOTAL" gcSum)] ++
-        [(-1, "")]
-      sparkLine :: Int -> RtsSparkCounters -> String
-      sparkLine k = displaySparkCounter (printf "SPARKS HEC %d" k)
-      sparkSum = sumSparkCounters $ IM.elems rtsSparks
-      sparkLines =
-        IM.assocs (IM.mapWithKey sparkLine rtsSparks) ++
-        [(100, displaySparkCounter "SPARKS TOTAL" sparkSum)] ++
-        [(200, "")]
-      timeLines =
-        [ (201, printf "  GC      time  %6.2fs elapsed"
-                  (timeToSecondsDbl (gcElapsed gcSum)))
-        , (202, printf "  Total   time  %6.2fs elapsed" totalElapsedS)
-        ]
-      infoLines = gcLines ++ sparkLines ++ timeLines
-      info = unlines $ map snd $ L.sort infoLines
-  in (info, Just events)
- where
-  eventBlockEnd e | EventBlock{ end_time=t } <- spec $ ce_event e = t
-  eventBlockEnd e = time $ ce_event e
-  -- Warning: stack overflow when done like in ReadEvents.hs:
-  lastTx =
-    L.foldl' (\ acc e -> max acc (eventBlockEnd e)) 1 (elems $ events)
-  (istart, iend) = fromMaybe (0, lastTx) minterval
-  tIME_RESOLUTION = 1000000
-  timeToSecondsDbl :: Integral a => a -> Double
-  timeToSecondsDbl t = fromIntegral t / tIME_RESOLUTION
-  sumGCCounters l =
-    let sumPr proj = L.sum $ L.map proj l
-    in RtsGCCounters
-         ModePar EndGC 0 (sumPr gcSeq) (sumPr gcPar) (sumPr gcElapsed)
-         (L.maximum $ 0 : map gcMaxPause l)
-  displayGCCounter :: String -> RtsGCCounters -> String
-  displayGCCounter header RtsGCCounters{..} =
-    let gcColls = gcSeq + gcPar
-        gcElapsedS = timeToSecondsDbl gcElapsed
-        gcMaxPauseS = timeToSecondsDbl gcMaxPause
-        gcAvgPauseS
-          | gcColls == 0 = 0
-          | otherwise = gcElapsedS / fromIntegral gcColls
-    in printf "  %s  Gen 0+1  %5d colls, %5d par      %5.2fs          %3.4fs    %3.4fs" header gcColls gcPar gcElapsedS gcAvgPauseS gcMaxPauseS
-  sumSparkCounters l =
-    let sumPr proj = L.sum $ L.map proj l
-    in RtsSparkCounters
-         (sumPr sparkCreated) (sumPr sparkDud) (sumPr sparkOverflowed)
-         (sumPr sparkConverted) (sumPr sparkFizzled) (sumPr sparkGCd)
-  displaySparkCounter :: String -> RtsSparkCounters -> String
-  displaySparkCounter header RtsSparkCounters{..} =
-    printf "  %s: %7d (%7d converted, %7d overflowed, %7d dud, %7d GC'd, %7d fizzled)" header (sparkCreated + sparkDud + sparkOverflowed) sparkConverted sparkOverflowed sparkDud sparkGCd sparkFizzled
-  step !state (CapEvent _ ev) | time ev < istart || time ev > iend = state
-  step !state (CapEvent mcap ev) =
-    let defaultGC time = RtsGCCounters
-          { gcMode = ModeInitial
-          , gcLastEvent = EndGC
-          , gcLastStart = time
-          , gcSeq = 0
-          , gcPar = 0
-          , gcElapsed = 0
-          , gcMaxPause = 0
-          }
-        -- We ignore GCWork, GCIdle and GCDone. Too detailed for the summary.
-        stateNew cap !rtsstate@RtsState{rtsGC, rtsSparks} (Event time spec) =
-         let defGC@RtsGCCounters{..} =
-               IM.findWithDefault (defaultGC time) cap rtsGC
-             _lastGcEnded RtsGCCounters{gcLastEvent} = case gcLastEvent of
-                                                         EndGC -> True
-                                                         _     -> False
-         in case spec of
-          -- TODO: check EventBlock elsewhere, define {map,fold}EventBlock, etc.
-          EventBlock {cap = bcap, block_events} ->
-            L.foldl' (stateNew bcap) rtsstate block_events
+defaultGC :: Timestamp -> RtsGC
+defaultGC time = RtsGC
+  { gcMode = ModeInitial
+  , gcLastEvent = EndGC
+  , gcLastStart = time
+  , gcSeq = 0
+  , gcPar = 0
+  , gcElapsed = 0
+  , gcMaxPause = 0
+  }
+
+scanEvents :: SummaryData -> CapEvent -> SummaryData
+scanEvents !summaryData (CapEvent mcap ev) =
+  let -- For events that contain a counter with a running sum.
+      -- Eventually we'll subtract the last found
+      -- event from the first. Intervals beginning at time 0
+      -- are a special case, because morally the first event should have
+      -- value 0, but it may be absent, so we start with @Just (0, 0)@.
+      alterCounter n Nothing = Just (n, n)
+      alterCounter n (Just (_previous, first)) = Just (n, first)
+      -- For events that contain discrete increments. We assume the event
+      -- is emitted close to the end of the process it measures,
+      -- so we ignore the first found event, because most of the process
+      -- could have happened before the start of the current inverval.
+      -- This is consistent with @alterCounter@. For interval beginning
+      -- at time 0, we start with @Just 0@.
+      alterIncrement _ Nothing = Just 0
+      alterIncrement n (Just k) = Just (k + n)
+      -- For events that contain sampled values, where a max is sought.
+      alterMax n Nothing = Just n
+      alterMax n (Just k) | n > k = Just n
+      alterMax _ jk = jk
+      -- Scan events, updating summary data.
+      scan cap !sd@SummaryData{..} Event{time, spec} =
+        let defGC@RtsGC{..} =
+              IM.findWithDefault (defaultGC time) cap dGCTable
+            _lastGcEnded RtsGC{gcLastEvent} =
+              case gcLastEvent of
+                EndGC -> True
+                _     -> False
+        in case spec of
+          -- TODO: check EventBlock elsewhere; define {map,fold}EventBlock
+          EventBlock{cap = bcap, block_events} ->
+            L.foldl' (scan bcap) sd block_events
+          HeapAllocated{allocBytes} ->
+            sd { dallocated = alterCounter allocBytes dallocated }
+          GCStatsGHC{copied, slop, frag} ->
+            sd { dcopied = alterIncrement copied dcopied
+               , dmaxSlop = alterMax slop dmaxSlop
+               , dmaxFrag = alterMax frag dmaxFrag  -- TODO
+               }
+          HeapLive{liveBytes} ->
+            sd { dmaxResidency = alterMax liveBytes dmaxResidency}
+          HeapSize{sizeBytes} ->
+            sd { dmaxMemory = alterMax sizeBytes dmaxMemory}
           RequestSeqGC ->
             -- All the other caps must stop, hence the assertion:
-            assert (L.all _lastGcEnded $ IM.elems rtsGC) $
-            rtsstate { rtsGC = IM.insert cap (defGC { gcMode = ModeSeq }) rtsGC
-                     }
+            assert (L.all _lastGcEnded $ IM.elems dGCTable) $
+            sd { dGCTable =
+                   IM.insert cap (defGC { gcMode = ModeSeq }) dGCTable }
           RequestParGC ->
-            -- RequestParGC is enough to distinguish
-            -- between seq and par GC; only one cap issues a RequestParGC,
-            -- the others will all StartGC at some point.
-            -- Unfortunately, RequestParGC may register after other caps
-            -- have already sent their StartGC events, so the following
-            -- does not hold in rare cases:
-            -- assert (L.all _lastGcEnded $ IM.elems rtsGC) $
-            rtsstate { rtsGC = IM.map (\ dGC -> dGC { gcMode = ModePar }) rtsGC
-                     }
+            sd { dGCTable =
+                   IM.map (\ dGC -> dGC { gcMode = ModePar }) dGCTable }
           StartGC ->
             assert (_lastGcEnded defGC) $
             let newGC = defGC { gcLastEvent = StartGC
-                              , gcLastStart = time }
-            in rtsstate { rtsGC = IM.insert cap newGC rtsGC
-                        }
+                              , gcLastStart = time
+                              }
+            -- TODO: Index with generations, not caps.
+            in sd { dGCTable = IM.insert cap newGC dGCTable }
           EndGC ->
-            -- TODO: not true for intervals, but check in 'ghc-events verify'
-            -- for the whole log::
-            -- assert (case gcLastEvent of
-            --           StartGC -> True
-            --           _       -> False) $
             let duration = time - gcLastStart
                 endedGC = defGC { gcMode = ModeEnded
                                 , gcLastEvent = EndGC
@@ -256,22 +238,125 @@ summaryViewProcessEvents minterval (Just events) =
                   -- the matching and so correctly register stats of the GC.
                   ModeEnded -> endedGC
             in case gcLastEvent of
-              StartGC -> rtsstate { rtsGC = IM.insert cap collsGC rtsGC
-                                  }
+              StartGC -> sd { dGCTable = IM.insert cap collsGC dGCTable }
               -- We don't know the exact timing of this GC started before
               -- the selected interval, so we skip it.
               -- We don't know if StartGC or RequestParGC/RequestSeqGC
               -- gets issued first,so we have to check both gcMode
               -- above and gcLastEvent here to correctly skip the GC.
-              _       -> rtsstate { rtsGC = IM.insert cap endedGC rtsGC
-                                  }
-          SparkCounters crt dud ovf cnv fiz gcd _rem -> -- TODO
-            let current = RtsSparkCounters crt dud ovf cnv fiz gcd
-                alter Nothing = Just (current, current)
-                alter (Just (first, _previous)) = Just (first, current)
-            in rtsstate { rtsSparks = IM.alter alter cap rtsSparks }
-          _ -> rtsstate
-    in stateNew (fromJust mcap) state ev
+              _       -> sd { dGCTable = IM.insert cap endedGC dGCTable }
+          SparkCounters crt dud ovf cnv fiz gcd _rem ->
+            -- We are guranteed the first spark counters event has all zeroes,
+            -- do we don't need to rig the counters for maximal interval.
+            let current = RtsSpark crt dud ovf cnv fiz gcd
+            in sd { dsparkTable =
+                      IM.alter (alterCounter current) cap dsparkTable }
+          _ -> sd
+    in scan (fromJust mcap) summaryData ev
+
+ppWithCommas :: Word64 -> String
+ppWithCommas =
+  let spl [] = []
+      spl l  = let (c3, cs) = L.splitAt 3 l
+               in c3 : spl cs
+  in L.reverse . L.intercalate "," . spl . L.reverse . show
+
+printW :: String -> Maybe Word64 -> [String]
+printW _ Nothing = []
+printW s (Just w) = [printf s $ ppWithCommas w]
+
+memLines :: SummaryData -> [String]
+memLines SummaryData{..} =
+  printW "%16s bytes allocated in the heap"
+    (fmap (uncurry (-)) dallocated) ++
+  printW "%16s bytes copied during GC" dcopied ++
+  printW "%16s bytes maximum residency" dmaxResidency ++
+  printW "%16s bytes maximum slop" dmaxSlop ++
+  printf ("%16d MB total memory in use "
+          ++ printf "(%d MB lost due to fragmentation)"
+                (fromMaybe 0 dmaxFrag `div` (1024 * 1024)))
+         (fromMaybe 0 dmaxMemory `div` (1024 * 1024)) :
+  [""]
+
+-- TODO: factor away gcLines, sparkLines, timeLines
+summaryViewProcessEvents :: Maybe Interval -> Maybe (Array Int CapEvent)
+                         -> (String, Maybe (Array Int CapEvent))
+summaryViewProcessEvents _ Nothing = ("", Nothing)
+summaryViewProcessEvents minterval (Just events) =
+  let start = if istart == 0
+              then -- Intervals beginning at time 0 are a special case,
+                   -- because morally the first event should have value 0,
+                   -- but it may be absent, so we start with 0.
+                   emptySummaryData { dallocated = Just (0, 0)
+                                    , dcopied = Just 0
+                                    }
+              else emptySummaryData
+      f summaryData CapEvent{ce_event=Event{time}}
+        | time < istart || time > iend = summaryData
+      f summaryData ev = scanEvents summaryData ev
+      sd@SummaryData{dsparkTable = dsparkTableRaw, ..} =
+        L.foldl' f start $ elems $ events
+      diffSparks (RtsSpark crt1 dud1 ovf1 cnv1 fiz1 gcd1,
+                  RtsSpark crt2 dud2 ovf2 cnv2 fiz2 gcd2) =
+        RtsSpark (crt1 - crt2) (dud1 - dud2) (ovf1 - ovf2)
+                 (cnv1 - cnv2) (fiz1 - fiz2) (gcd1 - gcd2)
+      dsparkTable = IM.map diffSparks dsparkTableRaw
+      totalElapsedS = timeToSecondsDbl $ iend - istart
+      gcLine :: Int -> RtsGC -> String
+      gcLine k = displayGCCounter (printf "GC HEC %d" k)
+      gcSum = sumGCCounters $ IM.elems dGCTable
+      -- TODO: sort by gen, not by cap
+      gcLines =
+        ["                                            Tot elapsed time   Avg pause  Max pause"] ++
+        IM.elems (IM.mapWithKey gcLine dGCTable) ++
+        [displayGCCounter "GC TOTAL" gcSum] ++
+        [""]
+      sparkLine :: Int -> RtsSpark -> String
+      sparkLine k = displaySparkCounter (printf "SPARKS HEC %d" k)
+      sparkSum = sumSparkCounters $ IM.elems dsparkTable
+      sparkLines =
+        IM.elems (IM.mapWithKey sparkLine dsparkTable) ++
+        [displaySparkCounter "SPARKS TOTAL" sparkSum] ++
+        [""]
+      timeLines =
+        [ printf "  GC      time  %6.2fs elapsed"
+            (timeToSecondsDbl (gcElapsed gcSum))
+        , printf "  Total   time  %6.2fs elapsed" totalElapsedS
+        ]
+      info = unlines $ memLines sd ++ gcLines ++ sparkLines ++ timeLines
+  in (info, Just events)
+ where
+  eventBlockEnd e | EventBlock{ end_time=t } <- spec $ ce_event e = t
+  eventBlockEnd e = time $ ce_event e
+  -- Warning: stack overflow when done like in ReadEvents.hs:
+  lastTx =
+    L.foldl' (\ acc e -> max acc (eventBlockEnd e)) 1 (elems $ events)
+  (istart, iend) = fromMaybe (0, lastTx) minterval
+  tIME_RESOLUTION = 1000000
+  timeToSecondsDbl :: Integral a => a -> Double
+  timeToSecondsDbl t = fromIntegral t / tIME_RESOLUTION
+  sumGCCounters l =
+    let sumPr proj = L.sum $ L.map proj l
+    in RtsGC
+         ModePar EndGC 0 (sumPr gcSeq) (sumPr gcPar) (sumPr gcElapsed)
+         (L.maximum $ 0 : map gcMaxPause l)
+  displayGCCounter :: String -> RtsGC -> String
+  displayGCCounter header RtsGC{..} =
+    let gcColls = gcSeq + gcPar
+        gcElapsedS = timeToSecondsDbl gcElapsed
+        gcMaxPauseS = timeToSecondsDbl gcMaxPause
+        gcAvgPauseS
+          | gcColls == 0 = 0
+          | otherwise = gcElapsedS / fromIntegral gcColls
+    in printf "  %s  Gen 0+1  %5d colls, %5d par      %5.2fs          %3.4fs    %3.4fs" header gcColls gcPar gcElapsedS gcAvgPauseS gcMaxPauseS
+  sumSparkCounters l =
+    let sumPr proj = L.sum $ L.map proj l
+    in RtsSpark
+         (sumPr sparkCreated) (sumPr sparkDud) (sumPr sparkOverflowed)
+         (sumPr sparkConverted) (sumPr sparkFizzled) (sumPr sparkGCd)
+  displaySparkCounter :: String -> RtsSpark -> String
+  displaySparkCounter header RtsSpark{..} =
+    printf "  %s: %7d (%7d converted, %7d overflowed, %7d dud, %7d GC'd, %7d fizzled)" header (sparkCreated + sparkDud + sparkOverflowed) sparkConverted sparkOverflowed sparkDud sparkGCd sparkFizzled
 
 summaryViewSetEvents :: SummaryView -> Maybe (Array Int CapEvent) -> IO ()
 summaryViewSetEvents = genericSetEvents (summaryViewProcessEvents Nothing)
