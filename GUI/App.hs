8b3c1ad9863619e179c5fb9a4942706fd70e8c54
Mitsutoshi Aoe <me@maoe.name>
2020-03-01 23:09:47 +0900

    Remove dependency on gtk-mac-integration
    gtk2hs/gtk-mac-integration has been unmaintained for nearly 3 years
    now and it makes it harder to build ThreadScope on macOS.
    
    We've been using it only to display the logo in the Dock so the
    effect of this removal is minimal. We can add this feature back
    when porting ThreadScope to gi-gtk.

--- GUI/App.hs
+++ GUI/App.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE TemplateHaskell #-}
-
 -------------------------------------------------------------------------------
 -- | Module : GUI.App
 --
@@ -9,33 +6,9 @@
 
 module GUI.App (initApp) where
 
--- Mac OS X-specific GTK imports
-#if defined(darwin_HOST_OS)
-import qualified Graphics.UI.Gtk as Gtk
-import qualified Graphics.UI.Gtk.OSX as OSX
-import GUI.DataFiles (loadLogo)
-#endif
-
 -------------------------------------------------------------------------------
-
-#if defined(darwin_HOST_OS)
-
--- | Initialize application
--- Perform Mac OS X-specific application initialization
-initApp :: IO ()
-initApp = do
-  app <- OSX.applicationNew
-  menuBar <- Gtk.menuBarNew
-  OSX.applicationSetMenuBar app menuBar
-  logo <- $loadLogo
-  OSX.applicationSetDockIconPixbuf app logo
-  OSX.applicationReady app
-
-#else
 
 -- | Initialize application
 -- Perform application initialization for non-Mac OS X platforms
 initApp :: IO ()
 initApp = return ()
-
-#endif
